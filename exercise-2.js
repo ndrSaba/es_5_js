87a3442860c1d7cc4a7f6b33cfc55b7e783046a6
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-06-21 22:09:39 +0200

    es fixato

--- exercise-2.js
+++ exercise-2.js
@@ -1,29 +1,30 @@
+function calculateSalary(role) {
+  switch (role) {
+    case `ceo`:
+      return "ceo salary = 2200€";
+    break; 
+    case `manager`:
+      return "manager salary = 1800€";
+    break;
+    case `cto`:
+      return "cto salary = 1800€";
+    break;
+    case `developer`:
+      return "developer salary = 1500€";
+    break; 
+    default:
+      return "other salaries = 1000€";
+  }
+}
+
 const ceoSalary = calculateSalary('ceo');
 const managerSalary = calculateSalary('manager');
 const ctoSalary = calculateSalary('cto');
 const developerSalary = calculateSalary('developer');
 const otherSalary = calculateSalary('other');
 
-
-function calculateSalary(role) {
-  
-  switch (role) {
-    case "ceo":
-      console.log("Ceo salary => 2200€");
-      break;
-    case "manager":
-      console.log("Manager salary => 1800€");
-      break;
-    case "cto":
-      console.log("Cto salary => 1800€");
-      break;
-    case "developer":
-      console.log("Developer salary => 1500€");
-      break;
-    default:
-      console.log("Other salary => 1000€");
-      break;
-  }
-}
-
-console.log(calculateSalary)
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
